6d904ba369700eebb48a6283190d4a36f3e77566
fhub-1 <flutterhub1.21.0@gmail.com>
2022-11-22 15:50:02 +0300

    adding chnages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+const { hostname } = require("os");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
   images: {
-    domains: ["assets.suncel.io"],
+    domains: ["assets.suncel.io", "next-suncel-736qmh16o-fhub-1.vercel.app"],
   },
 };
 
